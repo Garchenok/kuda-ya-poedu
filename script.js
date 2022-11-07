71f5d911a29484f124a45de06132e441bd70f454
Igor Garchenok <igorgarchenok@yandex.ru>
2022-11-07 11:12:09 +0300

    burger/media

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function closePopup() {
   popup.classList.remove("popup_opened");
 }
 popupClose.addEventListener("click", closePopup);
+
+// burger-report
+let reportButtonBurger = document.querySelector(".header__burger-report");
+let popupBurger = document.querySelector(".popup");
+let popupCloseBurger = document.querySelector(".popup__close");
+
+function openPopup() {
+  popupBurger.classList.add("popup_opened");
+}
+reportButtonBurger.addEventListener("click", openPopup);
+
+function closePopup() {
+  popupBurger.classList.remove("popup_opened");
+}
+popupCloseBurger.addEventListener("click", closePopup);
